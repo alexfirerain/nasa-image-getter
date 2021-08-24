4573cdd0e52c0e26ff62886935414a852c258c37
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-24 14:18:23 +0300

    description got, trying to get img

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,18 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static final String NASA_SOURCE = "https://api.nasa.gov/planetary/apod?api_key=";
     public final static String MY_KEY = "A0mxeLsHT8T9su74w00tjMKCtDbGYoBboK4jf3re";
-    private static ObjectMapper apodMapper = new ObjectMapper();
+    private static final ObjectMapper apodMapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
@@ -34,13 +37,72 @@ public class Main {
 
         CloseableHttpResponse response = httpClient.execute(request);
 
-        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+//        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
 //        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
 
         NasaApodResponse apod = apodMapper.readValue(response.getEntity().getContent(), NasaApodResponse.class);
         System.out.println(apod);
 
+        saveDescription(apod, "");
 
+//        apod.saveDescription("");
+//        apod.saveLowResImage("");
+//        apod.saveHiResImage("");
 
+    }
+
+    public static void saveDescription(NasaApodResponse apod, String whereToSave) {
+        String nameToSave = apod.namePatternForFiles() + " - description.txt";
+        String savePath = whereToSave + (whereToSave.isBlank() ? "" : "\\") + nameToSave;   //?
+        File save = new File(savePath);
+
+        StringBuilder content = new StringBuilder()
+                .append("\t# # # ").append(apod.getTitle()).append(" # # #\r\n")
+                .append("a picture of ").append(apod.getDate()).append("\r\n\r\n")
+                .append(apod.getExplanation()).append("\r\n");
+
+        if (save.exists()) {
+            System.out.println("Файл " + nameToSave + " уже существует.\n" +
+                    "Перезаписать? ('+' для подтверждения)");
+            Scanner input = new Scanner(System.in);
+            if (!input.nextLine().equals("+")) {
+                System.out.println("Описание не сохранено.");
+                return;
+            }
+        }
+        try (FileWriter writer = new FileWriter(savePath, false)) {
+            writer.write(content.toString());
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Ошибка сохранения " + nameToSave + ". " + e.getMessage());
+        }
+    }
+
+    public static void saveLowResImage(NasaApodResponse apod, String whereToSave) {
+        String nameToSave = apod.namePatternForFiles() + " - low resolution.jpg";
+        String savePath = whereToSave + (whereToSave.isBlank() ? "" : "\\") + nameToSave;
+        File save = new File(savePath);
+//        try
+//        {
+//            URL imageURL = new URL(apod.getUrl());
+//            HttpURLConnection imageConnection;
+//            imageConnection = (HttpURLConnection) imageURL.openConnection();
+//            imageConnection.setRequestMethod("GET");
+//            imageConnection.connect();
+//
+//            InputStream in = imageConnection.getInputStream();
+//            OutputStream writer = new FileOutputStream(save);
+//            byte[] buffer = new byte[buffSize];
+//            int c = in.read(buffer);
+//            while (c > 0) {
+//                writer.write(buffer, 0, c);
+//                c = in.read(buffer);
+//            }
+//            writer.flush();
+//            writer.close();
+//            in.close();
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
     }
 }
--- src/main/java/NasaApodResponse.java
+++ src/main/java/NasaApodResponse.java
@@ -1,13 +1,46 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class NasaApodResponse {
-    String  date,
+    private String  date,
             explanation,
             hdUrl,
             mediaType,
             serviceVersion,
             title,
             url;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getHdUrl() {
+        return hdUrl;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public String getServiceVersion() {
+        return serviceVersion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 
     public NasaApodResponse(
            @JsonProperty("date") String date,
@@ -29,15 +62,64 @@ public class NasaApodResponse {
     @Override
     public String toString() {
         return ("""
-                Ответ от NASA для APOD:
-                \tдата: %s
-                \tпояснение: %s
-                \tURL картинки с высоким разрешением: %s
-                \tтип медиа: %s
-                \tверсия сервиса: %s
-                \tназвание: %s
-                \tURL картинки с низким разрешением: %s
+               
+               * * * * * NASA Astronomical Picture Of a Day * * * * * *
+               *
+               *\t<версия сервиса: %5$s; тип медиа: %4$s>
+               *
+               *\tназвание: %6$s
+               *\tдата: %1$s
+               *\tпояснение: %2$s
+               *\tURL картинки с низким разрешением: %7$s
+               *\tURL картинки с высоким разрешением: %3$s
+               *
+               * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+                
                 """)
                 .formatted(date, explanation, hdUrl, mediaType, serviceVersion, title, url);
     }
+
+    public String namePatternForFiles() {
+        return "NASA picture of a day " + date;
+    }
+
+    public void saveDescription(String dirToSave) {
+        String nameToSave = namePatternForFiles() + " - description.txt";
+        String savePath = dirToSave + (dirToSave.isBlank() ? "" : "\\") + nameToSave;
+        File save = new File(savePath);
+
+        StringBuilder content = new StringBuilder()
+                .append("\t# # # ").append(title).append(" # # #\r\n")
+                .append("a picture of ").append(date).append("\r\n\r\n")
+                .append(explanation).append("\r\n");
+
+        if (save.exists()) {
+            System.out.println("Файл " + nameToSave + " уже существует.\n" +
+            "Перезаписать? ('+' для подтверждения)");
+            Scanner input = new Scanner(System.in);
+            if (!input.nextLine().equals("+")) {
+                System.out.println("Описание не сохранено.");
+                return;
+            }
+        }
+        try (FileWriter writer = new FileWriter(savePath, false)) {
+            writer.write(content.toString());
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Ошибка сохранения " + nameToSave + ". " + e.getMessage());
+        }
+    }
+
+    public void saveLowResImage(String dirToSave) {
+        String nameToSave = namePatternForFiles() + " - low resolution.jpg";
+        String savePath = dirToSave + (dirToSave.isBlank() ? "" : "\\") + nameToSave;
+        File save = new File(savePath);
+    }
+
+    public void saveHiResImage(String dirToSave) {
+        String nameToSave = namePatternForFiles() + " - high resolution.jpg";
+        String savePath = dirToSave + (dirToSave.isBlank() ? "" : "\\") + nameToSave;
+        File save = new File(savePath);
+    }
+
 }
